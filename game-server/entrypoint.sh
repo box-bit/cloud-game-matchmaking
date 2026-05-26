2d351364b6cf47b9951444923d0189114c6d070b
box-bit <alessandrofantesini@gmail.com>
2026-05-26 10:52:55 +0200

    fixed cfg file for game server

--- game-server/entrypoint.sh
+++ game-server/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -e
 
-cat > /home/redeclipse/.redeclipse/servinit.cfg << EOF
-servertype 3
-serverip 0.0.0.0
-serverport ${SERVER_PORT}
-maxplayers ${MAX_PLAYERS}
-serverdesc "${SERVER_DESC}"
-serverpublic ${SERVER_PUBLIC}
-adminpass "${ADMIN_PASS}"
-serverpass "${SERVER_PASS}"
-// 0 = deathmatch (free-for-all), no mutators
-mode 0
-mutators 0
-timelimit ${TIME_LIMIT}
-fraglimit ${FRAG_LIMIT}
+SERVERTYPE=$([ "$SERVER_PUBLIC" = "1" ] && echo 3 || echo 1)
+
+cat >/home/redeclipse/.redeclipse/servinit.cfg <<EOF
+serverpass "$SERVER_PASS"
+adminpass "$ADMIN_PASS"
+
+if (= \$rehashing 0) [
+    servertype $SERVERTYPE
+    serverport $SERVER_PORT
+]
+
+sv_serverclients $MAX_PLAYERS
+sv_serverdesc "$SERVER_DESC"
+sv_timelimit $(( TIME_LIMIT / 60 ))
+sv_pointlimit $FRAG_LIMIT
 EOF
 
 exec /usr/games/redeclipse-server -ss3
